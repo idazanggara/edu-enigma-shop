5379359f3578286ceb3ae703c47ea00029318def
idazanggara <idazideas@gmail.com>
2024-06-25 11:39:32 +0700

    belajar lifecycle

--- src/pages/Lifecycle/SubLifecycle.jsx
+++ src/pages/Lifecycle/SubLifecycle.jsx
@@ -3,6 +3,7 @@ import { Component } from 'react'
 class SubLifecycle extends Component {
   componentWillUnmount() {
     console.log("Called from componentWillUnmount")
+    // eslint-disable-next-line react/prop-types
     this.props.changeValue("Ngorok")
   }
   render() {
